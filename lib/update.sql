f8057d97e49dbd14e97bd73969935eb58e9941de
Chris Franklin <cf73021@gmail.com>
2018-10-02 18:36:58 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters (id, name, motto, species, author_id, series_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+UPDATE characters SET species = "Martian" WHERE id = 8
